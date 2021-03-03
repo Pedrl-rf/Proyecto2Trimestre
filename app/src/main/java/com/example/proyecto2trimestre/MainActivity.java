1ecbda034e653d8a6b369c208660e26ba0c88c3c
unknown <pedrl.rf2001@gmail.com>
2021-03-03 19:22:31 +0100

    Initial commit

---
+++ app/src/main/java/com/example/proyecto2trimestre/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyecto2trimestre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
