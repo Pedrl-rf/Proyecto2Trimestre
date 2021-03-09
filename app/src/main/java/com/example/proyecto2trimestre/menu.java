e9bcfa9db2f4d177417c17e0318aec2dfd431c93
Pedrl-rf <pedrl.rf2001@gmail.com>
2021-03-09 10:01:07 +0100

    Primera Vista- boton login

---
+++ app/src/main/java/com/example/proyecto2trimestre/menu.java
@@ -0,0 +1,12 @@
+package com.example.proyecto2trimestre;
+
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class menu extends AppCompatActivity {
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(R.layout.activity_main);
+    }
+}
