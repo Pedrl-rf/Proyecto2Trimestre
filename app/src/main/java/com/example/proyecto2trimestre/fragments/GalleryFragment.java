89e363b2b6a816d9391c29f1a63810deea8470fb
Pedrl-rf <pedrl.rf2001@gmail.com>
2021-03-16 12:49:06 +0100

    Hacer ContactFragment

--- app/src/main/java/com/example/proyecto2trimestre/fragments/GalleryFragment.java
+++ app/src/main/java/com/example/proyecto2trimestre/fragments/GalleryFragment.java
@@ -1,18 +1,16 @@
 package com.example.proyecto2trimestre.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.proyecto2trimestre.Menu;
 import com.example.proyecto2trimestre.R;
 import com.example.proyecto2trimestre.RecyclerAdapter;
