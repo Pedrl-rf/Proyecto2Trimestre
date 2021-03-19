7c4448c27d3ff552b3c72e38904a0ac91f66e6a6
Pedrl-rf <pedrl.rf2001@gmail.com>
2021-03-19 11:28:02 +0100

    boton salir

--- app/src/main/java/com/example/proyecto2trimestre/Home.java
+++ app/src/main/java/com/example/proyecto2trimestre/Home.java
@@ -1,13 +1,18 @@
 package com.example.proyecto2trimestre;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 
 import com.example.proyecto2trimestre.fragments.BlogFragment;
@@ -39,6 +44,8 @@ public class Home extends AppCompatActivity {
         mBottomNavigation.setSelectedItemId(R.id.menu_home);
     }
 
+
+
     private void navigateToFragment(int itemId) {
 
         Fragment fragment;
@@ -68,5 +75,28 @@ public class Home extends AppCompatActivity {
         transaction.replace(R.id.container, fragment);
         transaction.commit();
         //setTitle(title);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode ==event.KEYCODE_BACK){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("¿Deseas salir de la aplicacion?").setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent(Intent.ACTION_MAIN);
+                    intent.addCategory(Intent.CATEGORY_HOME);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+            })
+                    .setNegativeButton("no", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
